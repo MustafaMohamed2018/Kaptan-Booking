9fd07463b39661d4ea969b7f69bb3d7a760f2e3b
MustafaMohamed2018 <mustafa29796@gmail.com>
2023-07-13 22:34:56 +0300

    fixes

--- src/app/registered-icons.ts
+++ src/app/registered-icons.ts
@@ -1,22 +1,22 @@
 export const RegisteredIcons = [
     {
         name:'plan-up',
-        path:'../assets/icons/plan up.svg'
+        path:'./assets/icons/plan up.svg'
     },
     {
         name:'plan-down',
-        path:'../assets/icons/plan-down.svg'
+        path:'./assets/icons/plan-down.svg'
     },
     {
         name:'arrows',
-        path:'../assets/icons/arrows.svg'
+        path:'./assets/icons/arrows.svg'
     },
     {
         name:'calendar-clock',
-        path:'../assets/icons/calendar-clock.svg'
+        path:'./assets/icons/calendar-clock.svg'
     },
     {
         name:'users',
-        path:'../assets/icons/users.svg'
+        path:'./assets/icons/users.svg'
     },
 ]
